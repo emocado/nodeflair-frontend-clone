4dc25d1ab0ff3caf47bbbf3bd3792e76bda109e5
Wei Khiang <weikhiang92000@gmail.com>
2023-09-06 23:47:17 +0900

    added jobinfo component and update responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import JobCard from "./components/JobCard"
+import JobInformation from "./components/JobInformation"
 
 function App() {
 
   return (
-    <div>
-      <JobCard />
+    <div className="flex px-[15px] max-w-[1140px] mx-auto">
+      <div className="min-w-[250px] w-[427px] max-[1010px]:flex-grow">
+        <JobCard />
+      </div>
+      <JobInformation />
     </div>
   )
 }
---
+++ src/components/JobInformation.jsx
@@ -0,0 +1,13 @@
+const JobInformation = () => {
+  return (
+    <div className="min-[1010px]:block hidden flex-grow py-2.5 px-[5px]">
+      <div className="pr-[5px] overflow-x-hidden overflow-y-scroll h-[500px]">
+        <div className="px-5 py-[29px] shadow-md rounded-lg bg-white h-full">
+          JobInformation
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default JobInformation
